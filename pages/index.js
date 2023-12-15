52fcb309b0ad5cfb7e865eb512ee7e039184301a
Phung Nam <nampp@cystack.net>
2023-12-16 03:07:56 +0700

    complete auth fe

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron text-center bg-primary square">
+        Online Learning English
+      </h1>
+      <p>from next js...</p>
+    </>
+  );
+};
+
+export default Index;
