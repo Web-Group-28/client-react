2cb70e2bf6a4a795b436e56b8050b554c5fe1d0c
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-17 17:37:26 +0700

    update

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -1,43 +1,50 @@
+import styles from './Course.module.css';
 import Link from 'next/link';
 import axios from 'axios';
-import React from 'react';
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
 
 const Courses = () => {
-  const [courses, setCourses] = React.useState([]);
-  // Fetch courses data on component mount
-  React.useEffect(() => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
     axios.get('/api/courses')
       .then((response) => {
-        console.log('Courses', courses);
-        console.log('Got the response', response.data.data);
         setCourses(response.data.data);
+        setLoading(false);
       })
       .catch((error) => {
-        console.warn('There was an error!', error);
+        console.error('Error fetching courses:', error);
+        setLoading(false);
       });
   }, []);
-  return (
-    <html>
-      <head>
-        <title>COURSES</title>
-      </head>
-      <div>
-        <h1 className="jumbotron text-center bg-primary square">
-          Online Learning English: Courses
-        </h1>
-        <div>
-          {courses.map(course => {
-            return <div className="course-box">
-              <Link href={`/courses/${course.courseID}`} key ={course.courseID}>
-                {course.title}
-              </Link>
-            </div>
-          })}
-        </div>
-      </div>
-    </html>
-  );
+  
+  if (loading) {
+    return <div className={styles.loadingSpinner}></div>;
+  }
 
+  return (
+    <div>
+      <Head>
+        <title>Online Learning English: Courses</title>
+      </Head>
+      <h1 className="jumbotron text-center bg-primary square">
+        Online Learning English: Courses
+      </h1>
+      <div>
+        {courses.length > 0 ? (
+          courses.map((course) => (
+            <div className={styles.courseBox} key={course.courseID}>
+              <Link href={`/courses/${course.courseID}`}>{course.title}</Link>
+            </div>
+          ))
+        ) : (
+          <p>No courses available.</p>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
