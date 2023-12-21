5cc9d992f9ea714be9033d24e71cf7ed219e9b2a
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-21 22:50:01 +0700

    remove use file

--- pages/lesson/test.js
+++
@@ -1,9 +0,0 @@
-var tmpState = new Set(['button_1', 'button_2']);
-console.log("CLICK BEFORE:", tmpState);
-if (true) {
-   tmpState.delete(`button_1`);
-   console.log("CLICK AFTER:", tmpState);
-} else {
-   tmpState.add(`button_1`);
-   console.log("CLICK AFTER:", tmpState);
-}
