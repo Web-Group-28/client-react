072a2d90c207fe5c4e822aaaa81406aba2d08e50
nguyennam5902 <nguyenhainam05092002@gmail.com>
2023-12-21 22:48:16 +0700

    complete sentence quiz

--- pages/lesson/[...cid_lid].js
+++ pages/lesson/[...cid_lid].js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import styles from './lesson.module.css';
+const tmpState = new Set([]);
 
 async function getQuiz(courseID, lessonID) {
    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/courses/${courseID}/lessons/${lessonID}/exercises`);
@@ -66,13 +67,15 @@ const QuizContent = ({ quizData }) => {
    console.log(sentence);
    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
    const [userAnswer, setUserAnswer] = useState("");
+   const [userSentenceAnswer, setUserSentenceAnswer] = useState([]);
+   const [sentenceButtonStates, setSentenceButtonStates] = useState({});
    const [showResult, setShowResult] = useState(false);
    const [correct, setCorrect] = useState(0);
    const [click, setClick] = useState(0);
    const [isEnabled, setEnabled] = useState(false);
    var correctAnswer = ""
 
-   const handleSubmit = () => {
+   const handleChoiceSubmit = () => {
       console.log("CLICKED:", click);
       const submitButton = document.querySelector(`.${styles.submitButton}`);
       const submitResult = document.querySelector(`.${styles.submitResult}`);
@@ -113,10 +116,61 @@ const QuizContent = ({ quizData }) => {
          if (currentQuestionIndex === choice.length + sentence.length - 1) {
             setShowResult(true);
          }
+      }
+      setClick(click + 1);
+   };
+   const handleSentenceSubmit = () => {
+      console.log("CLICKED:", click);
+      const submitButton = document.querySelector(`.${styles.submitButton}`);
+      const submitResult = document.querySelector(`.${styles.submitResult}`);
+      const bottomBar = document.querySelector(`.${styles.bottomBar}`);
+      if (click % 2 == 0) {
+         // Your submission logic here
+         submitButton.textContent = "Tiếp tục"
+         if (Array.from(tmpState).map(id => {
+            return document.getElementById(id).innerText;
+         }).join(' ') === correctAnswer) {
+            console.log("CORRECT");
+            submitResult.textContent = 'Làm tốt lắm!';
+            submitResult.style.color = `#58a700`
+            submitButton.style.backgroundColor = '#58cc02';
+            bottomBar.style.backgroundColor = '#d7ffb8';
+            setCorrect(correct + 1);
+            console.log(correct);
+         } else {
+            console.log("FALSE");
+            submitResult.textContent = `Đáp án đúng:`
+            document.querySelector(`.${styles.submitAnswer}`).textContent = correctAnswer;
+            submitResult.style.color = `#ea2b2b`
+            submitButton.style.backgroundColor = '#ff4b4b';
+            bottomBar.style.backgroundColor = '#ffdfe0';
+         }
+      } else {
+         document.querySelector(`.${styles.submitAnswer}`).textContent = '';
+         tmpState.forEach(id => {
+            document.getElementById(id).style.background = '#ffffff';
+         });
+         tmpState.clear();
+         document.querySelector('.answerText').textContent = ''
+         bottomBar.style.backgroundColor = '#ffffff';
+
+         submitButton.style.backgroundColor = '#e5e5e5';
+         submitButton.style.color = '#afafaf';
+         submitResult.textContent = "";
+         submitButton.textContent = "Kiểm tra"
+         setEnabled(false);
+         // Move to the next question
+         setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+
+         // Check if it's the last question
+         if (currentQuestionIndex === choice.length + sentence.length - 1) {
+            setShowResult(true);
+         }
 
       }
       setClick(click + 1);
    };
+
 
    const renderQuizContent = () => {
       if (currentQuestionIndex < choice.length) {
@@ -130,14 +184,14 @@ const QuizContent = ({ quizData }) => {
                <p style={{ marginTop: '50px', textAlign: 'center', fontSize: '19px' }}>{currentQuestion.question}</p>
                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                   {currentQuestion.answers.map((answer, index) => (
-                     <button key={index} className={styles.answerButton} onClick={() => handleOptionSelect(answer)}>
+                     <button key={index} className={styles.answerButton} onClick={() => handleChoiceOptionSelect(answer)}>
                         {answer}
                      </button>
                   ))}
                </div>
                <div className={styles.bottomBar}>
                   <h2 className={styles.submitResult}></h2>
-                  <button disabled={!isEnabled} className={styles.submitButton} onClick={handleSubmit}>
+                  <button disabled={!isEnabled} className={styles.submitButton} onClick={handleChoiceSubmit}>
                      Kiểm tra
                   </button>
                </div>
@@ -146,7 +200,10 @@ const QuizContent = ({ quizData }) => {
 
       } else {
          if (showResult) {
-            return <p>Correct: {correct} / {choice.length}</p>;
+            return <div>
+               <p>Correct: {correct} / {choice.length + sentence.length}</p>
+               <a href='/courses'> Return to courses</a>
+            </div>;
          }
          const currentQuestion = sentence[currentQuestionIndex - choice.length];
          correctAnswer = currentQuestion.correct
@@ -156,18 +213,19 @@ const QuizContent = ({ quizData }) => {
             <div style={{ position: 'relative', marginTop: '80px', display: 'flex', flexDirection: 'column', height: '650px' }}>
                <p style={{ marginLeft: '475px', textAlign: 'left', fontSize: '32px', fontWeight: 'bold' }}>{"Viết lại bằng tiếng Việt"}</p>
                <p style={{ marginTop: '50px', textAlign: 'center', fontSize: '19px' }}>{currentQuestion.left_sentence}</p>
-               <div style={{ alignSelf: 'center', width: '60%', marginTop: '99px', display: 'flex', flexDirection: 'row', alignItems: 'center' }}></div>
+               <div className='answerText' style={{ alignSelf: 'center', width: '60%', marginTop: '99px', display: 'flex', flexDirection: 'row', alignItems: 'center' }}></div>
                <div style={{ alignSelf: 'center', width: '60%', marginTop: '1px', borderTop: '2px solid black', display: 'flex', flexDirection: 'row', alignItems: 'center' }}></div>
                <div style={{ marginTop: '60px', display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
                   {currentQuestion.words.map((answer, index) => (
-                     <button key={index} className={styles.answerButton} onClick={() => handleOptionSelect(answer)}>
+                     <button key={index} id={`button_${index}`} className={styles.sentenceButton} onClick={() => handleSentenceOptionSelect(answer, index, tmpState)}>
                         {answer}
                      </button>
                   ))}
                </div>
                <div className={styles.bottomBar}>
                   <h2 className={styles.submitResult}></h2>
-                  <button disabled={!isEnabled} className={styles.submitButton} onClick={handleSubmit}>
+                  <div className={styles.submitAnswer}></div>
+                  <button disabled={!isEnabled} className={styles.submitButton} onClick={() => handleSentenceSubmit()}>
                      Kiểm tra
                   </button>
                </div>
@@ -177,14 +235,42 @@ const QuizContent = ({ quizData }) => {
       }
    };
 
-   const handleOptionSelect = (selectedOption) => {
+   const handleChoiceOptionSelect = (selectedOption) => {
       setUserAnswer(selectedOption);
       const s = document.querySelector(`.${styles.submitButton}`);
       setEnabled(true);
       s.style.backgroundColor = '#58cc02';
       s.style.color = '#ffffff';
    };
+   const handleSentenceOptionSelect = (selectedOption, index, tmpState) => {
+      console.log("CLICK BEFORE:", tmpState);
+      console.log("ANSWER:", userSentenceAnswer);
+      const clickedButton = document.querySelector(`#button_${index}`);
+      if (!sentenceButtonStates[selectedOption]) {
+         clickedButton.style.backgroundColor = '#e5e5e5';
+         tmpState.add(`button_${index}`);
+         setUserSentenceAnswer(old => old.filter(o => o !== `button_${index}`));
+      } else {
+         tmpState.delete(`button_${index}`);
+         clickedButton.style.backgroundColor = '#ffffff';
+         setUserSentenceAnswer(old => [...old, `button_${index}`]);
+      }
+      setSentenceButtonStates((prevStates) => ({
+         ...prevStates,
+         [selectedOption]: !prevStates[selectedOption],
+      }));
 
+      const s = document.querySelector(`.${styles.submitButton}`);
+      setEnabled(true);
+      s.style.backgroundColor = '#58cc02';
+      s.style.color = '#ffffff';
+      console.log("CLICK AFTER:", tmpState);
+      const answer = Array.from(tmpState).map(id => {
+         return document.getElementById(id).innerText;
+      });
+      const text = document.querySelector('.answerText');
+      text.textContent = answer.join(' ');
+   };
    return (
       <div>
          {renderQuizContent()}
